bbf1911bb7fc423440f513b528e14885e39da72c
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-09 17:49:27 +0500

    add data types lesson

--- intro.py
+++ intro.py
@@ -1,4 +1,16 @@
-print("Hello world !")
+"""
+Introduction to Python 3
+Editor VS Vode
+Learn python : 3
+
+"""
+
+print("Hello world !")  # chop etish funksiyasi
+
+# print "Строка 1"  # python 2
+# print("String 1")  # python 3
+input()
+
 
 # Interpretator - kodni tori yozilganini tekshiruvchui dastur
 # Kompilyator - kodni mashina tiliga o'giruvchi dastur
