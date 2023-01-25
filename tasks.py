ac179ac6d9e43c468e09d55b362fe54727d9783a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-25 17:42:44 +0500

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,15 @@
+# task 1
+# 10 bilan 35 orasida tasodifiy 5 ta sodan iborat massiv hosil qiling
+
+# task 2
+# task 1 taskni list generator orqali qiling
+
+
+# task 3
+# massivda turli turdagi elementlar bor massivni saralab barcha elementlarni turi boyicha saralab yozing
+
+# arr = [True, 1,1.3,3.3,"str", "qale"]
+# bools = [True]
+# ints =  [1]
+# floats = [1.3,3.3] 
+# strs = ["str", "qale"]
