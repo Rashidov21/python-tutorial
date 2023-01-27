33526c1a396e77c2741148a578096d4510294d97
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-01-27 17:16:31 +0500

    update lesson

--- dicts.py
+++ dicts.py
@@ -16,17 +16,61 @@
 # v = [12,5]
 
 # print(dict(zip(k,v))) # {'monkey': 12, 'elephant': 5}
-import random
-from faker import Faker
+# import random
+# from faker import Faker
 
-fake = Faker()
+# fake = Faker()
 
-users = {}
+# users = {}
 
-for i in range(10):
-    name = fake.name()
-    temp = {
-        f'{name.split(" ")[0].lower()}': random.randint(18,30)
-    }
-    users.update(temp)
-print(users)
+# for i in range(10):
+#     name = fake.name()
+#     temp = {
+#         f'{name.split(" ")[0].lower()}': random.randint(18,30)
+#     }
+#     users.update(temp)
+# print(users)
+# d = {}
+
+# d["name"] = "John Doe"
+
+# print(d) # {'name': 'John Doe'}
+
+# print(d.get("name")) # John Doe
+
+# print(d.setdefault("name", "No name")) # John Doe
+# print(d.setdefault("age", 20)) # 20
+
+# print(len(d)) # 2
+# del d["name"] # element ochirish
+
+# print(d) # {'age': 20}
+
+d = {}
+
+d["name"] = "John Doe"
+d["age"] = 20
+
+# for item in d.keys(): # faqat kalitlarini olish
+#     print(item)
+
+# for item in d.items(): # elementlarini olish
+#     print(item)
+
+# for item in d.values(): # faqat qiymatlarini olish
+#     print(item)
+# d.pop("age")  
+# print(d) # {'name': 'John Doe'}
+
+# print(d.popitem()) #('age', 20)
+
+# d.clear()
+# d.update({"salary":300}) #{'name': 'John Doe', 'age': 20, 'salary': 300}
+# print(d)
+
+# keys = "abc"
+# values = [1,2,3]
+
+# d = {k: v for (k, v) in zip(keys, values)}
+# print(d) #{'a': 1, 'b': 2, 'c': 3}
+
--- tasks.py
+++ tasks.py
@@ -13,3 +13,25 @@
 # ints =  [1]
 # floats = [1.3,3.3] 
 # strs = ["str", "qale"]
+
+# task 4
+# faker orqali 30 ta user ismini oling ismda "a" harfi 2 marta ishtirok etganlarini alohida 
+# dict qilib yozing kalit sifatida ismni o'zini qiymat sifatida random son ishlating
+
+# task 5 
+# user telefon raqam kiritadi uni togri yoki notogri kiritganini aniqlovchi dastur tuzing
+# +99 (893) 333-33-33
+
+
+# task 6
+# while orqali userdan kamida 5 ta sana qabul qiling
+# agar sana haqiqatda to'g'ri sana bo'lsa uni massivga yozing aks holda davom eting
+# d = "12.36.2012" >> False
+# d = "12.30.2012" >> True
+
+# task 7
+#  Odil har kuni muntazam 9 soatdan uxlashni reja qilgan. Siz uning uchun u soat nechida uxlashga
+#  yotsa roppa rosa 9 soatdan keyin soat nechi bo’lishini hisoblaydigan dastur yozishingiz kerak.
+#  Agar Tohir soat 22:00 da uhlashga yotsa demak uyg’onganida soat 07:00 bo’lishini ko’rsatishingiz kerak.
+# Kirish : ‘22:00’
+# Chiqish : ‘07:00’
