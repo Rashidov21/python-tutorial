b10a5d780014266b5f0e2a3dca574ee6f4a40e49
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-02-15 17:50:47 +0500

    update

---
+++ error_catch.py
@@ -0,0 +1,28 @@
+#1-Synatx errors
+#2-Logical errors
+#3-In proccess errors
+
+# BaseException - xatoliklar ota klasi 
+
+# try:
+#     # xatolik bolishi mumkin bolgan kod 
+# except:
+#     # xatolik topilsa ishlashi kerak bolgan kod 
+# else:
+#     # xatolik topilmasa ishlashi kerak bolgan ko 
+# finally:
+#     # xatolik topilsa topilmasa ishlashi kerak bolgan kod 
+
+
+
+# try:
+#     x = 1 / 1
+#     print(x)
+# except ZeroDivisionError as error:
+#     print(error)
+# else:
+#     print("xato yoq .kod dalshe ishlayapti")
+# finally:
+#     print("farqi yoq ishledi")
+    
+
--- mini-projects/web/pexels_api.py
+++ mini-projects/web/pexels_api.py
@@ -1,3 +1,40 @@
 import requests
+import io
+from PIL import Image
+# import pprint
 
 API = "AO5zZivgo9rnloJr590042Ferd3RDNWTGP9xB0SUvZLw2CrVXlNdD7a9"
+
+def download(q, count):
+    i = 1
+    while i <= int(count): 
+        PARAMS = {"page":f"{i}"}
+        HEADERS = {
+            "Authorization":"AO5zZivgo9rnloJr590042Ferd3RDNWTGP9xB0SUvZLw2CrVXlNdD7a9"
+        }
+        url = f"https://api.pexels.com/v1/search?query={q}&per_page=1"
+        
+        page = requests.get(url=url, headers=HEADERS, params=PARAMS)
+        # print(page.text)
+        response = page.json()
+        # print(response)
+        image_url = response.get("photos")[0].get("src").get("large")
+        obj = requests.get(image_url)
+
+        image = Image.open(io.BytesIO(obj.content))
+        # print(f"./media/{q}_{i}.jpeg")
+        path = "media/" + q + ".jpeg"
+        image.save(path)
+        # wget.download(url=image_url, out=f"./media/{q}_{i}.{image_url.split('.')[-1]}") # media/fox_1.jgep
+        
+        i += 1
+    # pprint.pprint(page.json())
+
+
+
+def main():
+    q = input("Query \n:")
+    count = input("Count \n:")
+    
+    download(q,count)
+main()
