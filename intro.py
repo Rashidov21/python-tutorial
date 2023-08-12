6612244484f0b5cbdb1af0f73212c103615b449a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-08-12 17:25:28 +0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,3 +1,53 @@
-# Dasturlash tili -  
-# Interpretator - 
-# Kompilyator - 
+# Dasturlash tili -  tarjimon 
+# Interpretator - ona tili o'qituvchisi
+# Kompilyator -  tarjimon dastur
+
+# Variables 
+
+# variable - RAM xotira katakchasiga olib boruvchi yol
+
+# 1. ingliz tilida 
+# 2. mantiqan to'g'ri , kichik harflar bilan 
+# 3. birinchi belgisi raqam bo'lishi mumkin emas 
+# 4. harflar , sonlar va _ dan boshqa belgi mumkin emas 
+#  5. zaxiralangan so'zlar bilan variable ochish mumkin emas
+
+# import keyword
+# print(keyword.kwlist)
+
+# _ = 1
+# _for = 10
+# print(_for * 2) # 20
+
+# l  = [_ for _ in range(5)]
+# print(l) # [0, 1, 2, 3, 4]
+# f1 = "Michael Shumacher"
+
+# uh = 1.80
+# user_height = 1.80
+# userHeight = 1.80
+# USER_HEIGHT = 1.80
+# print(USER_HEIGHT + 2)# 3.8
+# x = 1
+
+# Data Type - ma'lumot turi , xotira katakchasida saqlash mumkin bo'lgan ma'lumot birligi 
+
+# 1. bool - True , False 
+# 2. None - None 
+# 3. int - 1, -1
+# 4. float  - 1.0, -1.0
+# 5. complex - 2n 
+# 6. str - "s", 's' , """s"""
+# 7. bytes  
+# 8. bytearray
+
+# Data Structure - xotira katakchasida saqlash mumkin bo'lgan ma'lumotlar tuzilmasi 
+
+# 1. list - []
+# 2. tuple  - ()
+# 3. dict  - {}
+# 4. set - {}
+# 5. frozenset - frozenset()
+# 6. range - range(10)
+
+# function , type , module , ellipsis 
