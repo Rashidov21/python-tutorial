8f7c8c21ddde05549cfa12e6368eec6561c3d169
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-08-18 07:57:36 +0500

    update

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# if , elif , else - shartlarni tekshirish instruktsiyalari 
+# condition - holat 
+# x = int(input("X  = "))
+# y = int(input("Y  = "))
+# # print(x > y) # 2 
+
+# if x > y: # agar
+# 	print("Rost , x katta")
+# 	x = 1
+# 	print(x + 2) # 3
+# elif x == y:
+#     print("Ikkalasi teng")
+# else: # aks  holda
+#     print("Yolgon y katta")
+
+# task 1 
+# userdan familya qabul qiling va uni jinsini ekranga chiqaring
+# surname = input("Familya ? ")
+# if surname.endswith("va"):
+#     print("Ayol")
+# else:
+#     print("Erkak")
--- intro.py
+++ intro.py
@@ -51,3 +51,91 @@
 # 6. range - range(10)
 
 # function , type , module , ellipsis 
+
+
+# print(2+2) # 4
+# print(2-2) # 0
+# print(4/2) # 2.0
+# print(4//2) # 2
+# print(7%3) # 1
+# print(7*2) # 14
+# print(3**2) # 9 
+
+# print(2 * 2 / (3 + 1.5)) # 0.8888888888888888
+# print(2 * 2 // (3 + 1.5)) # 0.0
+
+# in - ichida , mavjudlikga tekshirish operatori 
+# print("s" in "salom") # True
+# print("s" in "xayr") # False
+
+# not - emas , inkor operatori 
+# print(not True) # False 
+# print(not 0) # True
+# print(not 1) # False
+
+# print("s" not in "xayr") # True
+# variant 1 
+# x = 5
+# x += 5
+# print(x) # 10
+# # variant 2 
+# x = 5
+# x = x + 5
+# print(x) # 10
+
+
+# print(2 == 2) # True
+# print(1 != 2) # True
+# print(1 > 0) # True
+# print(1 < 0) # False 
+
+# x = 1
+# y = 1 
+# z = 0
+# print(x + y) # 2
+
+# print(x is z) # False
+# print(x is y) # True
+# print(x is not z) # True
+
+# and - mantiqiy VA 
+# print("p" and 1 and -6 and 1) # 1
+# or - mantiqiy YOKI 
+# print(0 or "1" or -6 or -1) # 1
+
+# 27, 24, 25, 26, 22, 21
+# task 21
+# x=int(input('X>>'))
+# y=int(input('Y>>'))
+
+# if x==0 and y==0:
+#     print(0)
+# elif x==0:
+#     print(1)
+# elif y==0:
+#     print(2)
+# else:
+#     print(3)
+
+# task 22
+# x=int(input('X>>'))
+# y=int(input('Y>>'))
+
+# if x>0 and y>0:
+#     print(1)
+# elif x<0 and y>0:
+#     print(2)
+# elif x<0 and y<0:
+#     print(3)
+# else:
+#     print(4)
+
+
+# task 24
+x=int(input('X>>'))
+f=0
+if x>0:
+    f=2*sin(x)
+else:
+    f=x-6
+print(f)
