33ae19f8655203ff339aed9bb8629be7ad4485ee
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2023-09-26 17:25:13 +0500

    Create work_with_time.py

---
+++ work_with_time.py
@@ -0,0 +1,51 @@
+import datetime # sana va vaqt moduli
+import time  # vaqt moduli
+import calendar # kalendar moduli
+import locale
+locale.setlocale(locale.LC_ALL, "UZ_uz")
+# _now = datetime.datetime.now() 
+# print(_now) # 2023-09-26 16:39:01.826321
+# print(_now.year)
+# print(_now.month)
+# print(_now.day)
+# print(_now.hour)
+# print(_now.minute)
+# print(_now.second)
+# print(_now.microsecond)
+
+# br_year = 2006
+# br_month = 10
+# br_day = 10
+# print(_now.year - br_year)
+# print(_now.month - br_month)
+# print(_now.day - br_day)
+
+# _now = datetime.datetime.now() 
+# birthday = datetime.date(int(input("Year")),int(input("Month")),int(input("Day"))) 
+
+# days = datetime.date(_now.year,_now.month,_now.day) - birthday
+# days = days.days
+
+# year = days // 365
+# ots_month = days % 365
+# month  = (year * 12) + (ots_month // 12) 
+# print(month) 
+# print(f"You live :   year={year} , months={month} , and days={days}")
+ 
+# from datetime import time
+
+# print(time.strftime("%Y")) # string format time >> 2023 
+# print(time.strftime("%y")) # >> 23 
+# print(time.strftime("%m")) # >> oy raqami  
+# print(time.strftime("%b")) # >> oy nomi qisqa Sep 
+# print(time.strftime("%B")) # >> oy nomi toliq September  
+# print(time.strftime("%a")) # >> Tue kun nomi qisqa  
+# print(time.strftime("%A")) # >> Tuesday kun nomi toliq  
+# print(time.strftime("%M")) # >> minute 
+# print(time.strftime("%H")) # >> soat
+# print(time.strftime("%S")) # >> sekund
+# print(time.strftime("%x")) # >> 09/26/23
+# print(time.strftime("%X")) # >> 17:20:55
+
+text = "Bugun %d - %B , %Y - yil %A.  Soat %H dan %M minut o'tdi."
+print(time.strftime(text)) # Bugun 26 - Sentabr , 2023 - yil seshanba.  Soat 17 dan 24 minut o'tdi.
