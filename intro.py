9c67358321ef31e43088a2df60bafb5a101d9125
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-06-18 22:21:11 +0500

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,11 @@
+# Interpretator - kodni xatoliklarga tekshiruvchi dastur  
+# Kompilyator - kodni mashina tiliga o'giruvchi dastur
+
+print("Hello world")
+print(2+2)
+
+# RAM - Random Access Memory  
+# ROM - Read Only Memory 
+
+import random 
+random.randint() # 2 ta sonni orasidan tasodify bitta son qaytaradi 
