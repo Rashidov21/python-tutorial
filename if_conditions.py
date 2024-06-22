e9eb504422f028a87fe3aa7359ff3a5f7ecbff90
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-06-22 22:09:55 +0500

    2 lesson add

---
+++ if_conditions.py
@@ -0,0 +1,70 @@
+# age = 5
+# if age > 20:
+#     print("Ok")
+# elif age == 15:
+#     print("15 yosh")
+# elif age == 10:
+#     print("10 yosh")
+# else:
+#     print("yosh kichik")
+    
+# if <shart>: Mantiqiy Agar 
+#     shart bajarilsa ishlaydigan kod 
+# elif boshqa shart: Aks holda yoki
+#     shart bajarilsa ishlaydigan kod
+# else: Aks holda 
+#     shart bajarilmasa ishlaydigan kod  
+
+# username = "luke"
+# if username == "luke":
+#     print("welcome luke skywalker")
+# if username == "master":
+#     print("welcome master")
+# if username == "chui":
+#     print("welcome chui")
+    
+# username = "luke"
+# # nested if
+# if username == "luke" or username == "master" or username == "chui":
+#     if username == "luke":
+#         print("welcome luke skywalker")
+#     if username == "master":
+#         print("welcome master")
+#     if username == "chui":
+#         print("welcome chui")
+    
+# if True:
+#     x = 10
+#     y = 2
+#     if 10 == x + y:
+#         print("Salom")
+        
+# agar username == "luke"
+# agar username == "master"
+# agar username == "chui"
+
+
+# num = int(input("num \n"))
+# if num % 2 == 0:
+#     print("Juft son")
+# else:
+#     print("Toq son")
+
+# ternary operator  
+# shart bajarilsa qaytadigan natija if <shart> else shart bajarilmasa qaytadigan natija
+# num = int(input("num \n"))
+# result = "Juft" if num % 2 == 0 else "Toq"
+# print(result)
+
+# oddiy if else 
+# season = "yoz"
+# if season == 'yoz':
+#     print("issiq")
+# else:
+#     print("sovuq")
+# ternar if else 
+# weather = 'issiq' if season == 'yoz' else 'sovuq'
+# print(weather)
+# 3-variant 
+# weather = ('issiq', 'sovuq')[season == 'yoz']
+# print(weather)
---
+++ loops.py
@@ -0,0 +1,50 @@
+# for , while 
+# for - sanoqli takrorlanish 
+# while - to'xtovsiz takrorlanish 
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+
+# for <sanoq boshi o'zgaruvchi elon qilinadi> in <ketma ketlik>:
+#     bu yerda takrorlanishi kerak bolgan kod 
+
+# for i in "salom":
+#     print(i)
+
+# range - sonlar diapazoni 
+# nums = range(10) # 0 dan 10 gacha sonlar 
+# print(nums) # range(0, 10)
+# nums = range(2,15) # 2 dan 15 gacha sonlar 
+# print(nums) # range(2, 15)
+# nums = range(5,15,2) # 5 dan 15 gacha sonlar 2 tadan orada tashlab 
+# print(nums) # range(5, 15, 2)
+# for i in range(10):print(i, end="") # 0123456789
+# for i in range(2,15):print(i, end="") # 234567891011121314
+# for i in range(5,15,2):print(i, end="") # 5791113
+
+# for i in range(1,11):
+#     print(i)
+
+# i - iteration > takrorlanish
+# for watermelon in "fruits":
+#     print(watermelon)
+
+# i , k , j , x - kabi o'zgaruvchilarni elon qilish afzal
+# for i,k in [("a","b"),("c","d")]:
+#     print(i,k)
+
+# nums = []
+# for i in range(1,21):
+#     if i % 2 == 0:
+#         nums.append(i)
+        
+# print(nums) # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# print([i for i in range(1,21) if i % 2 == 0]) # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("takrorlanish tugadi")
