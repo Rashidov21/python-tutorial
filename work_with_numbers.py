124bef4a7b361d927fc6b511bac666996c898011
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-06-25 21:50:43 +0500

    Update work_with_numbers.py

--- work_with_numbers.py
+++ work_with_numbers.py
@@ -45,3 +45,36 @@ import random
 # print(round(12.5)) # 12
 # print(round(12.99)) # 13
 # print(round(12.1)) # 12
+
+# random 
+
+# print(random.random()) # 0 bilan 1 orasidagi bitta tasodify sonni qaytaradi
+# print(random.randint(1,10)) # berilgan 2 son orasidagi bitta tasodify sonni qaytaradi
+
+# print(random.choice("python")) # berilgan ketma-ketlikdan bitta tasodify elementni  qaytaradi
+# print(random.choice([1,2,3,4])) # berilgan ketma-ketlikdan bitta tasodify elementni  qaytaradi
+# a = [1,2,3,4,5]
+# random.shuffle(a) # berilgan ketma-ketlik elementlarini joylarini tasodify tarzda almashtirish
+# print(a)
+
+# berilgan ketma-ketlikdan korsatilgan miqdorda elementlarni olib massiv qilib qaytaradi 
+# print(random.sample("Python programming language", 5))
+
+
+# Password generator 
+# numbers = "0123456789" # 0-9
+# letters_low = "absdefjklmnoprstqufhyzxwvir" # a-z
+# letters_up = letters_low.upper() # A-Z
+
+# letters = numbers + letters_up + letters_low
+
+# password_lenght = int(input("Parolda nechta belgi bo'lishi kerak ?"))
+
+# if password_lenght > 6:
+#     password = ""
+#     for i in range(password_lenght):
+#         random_letter = random.choice(letters)
+#         password += random_letter
+#     print(f"Sizning parolingiz : {password}")
+# else:
+#     print("Parol 6 ta belgidan kam bo'lishi mumkin emas !")
